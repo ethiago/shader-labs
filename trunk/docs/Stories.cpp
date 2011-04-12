8b83cacf846b72983ac1457b7c26361df0395bbb
e.thiago.06@gmail.com <e.thiago.06@gmail.com@23c9b60f-9f39-85ba-2a90-a2d20fce122a>
2011-04-12 01:27:42 +0000

    Adicionando a Arvore completa das Stories na documentação

---
+++ trunk/docs/Stories.cpp
@@ -0,0 +1,150 @@
+void Stories() {
+    17;
+    "Eu, enquanto usuario, gostaria de visualizar os resultados de um codigo GLSL \
+     com o bjetivo de fazer testes e experimentos com algoritmos de forma simples.";
+    {
+         3.2;
+        "Eu, enquanto usuario, gostaria de testar codigo para Vertex Shader.";
+        {
+            "Eu, enquanto usuario, gostaria de abrir um arquivo com codigo GLSL\
+             com o objetivo de testa-lo.";
+            {
+                "Eu, enquanto usuario, gostaria de selecionar um arquivo atraves\
+                 de uma interface amigavel com o objetivo de encontra-lo mais\
+                 facilmente.";
+                
+                "Eu, enquanto usuario, gostaria que o codigo fosse mostrado com\
+                 o objetivo de edita-lo.";
+
+                "Eu, enquanto usuario, gostaria de poder criar um novo arquivo\
+                 para codigo de dentro do programa, com o objetivo de testar\
+                 novas ideias.";
+                
+                "Eu, enquanto usuario, gostaria de poder salvar um arquivo\
+                 criado ou editado dentro do programa.";
+            }
+            
+            "Eu, enquanto usuario, gostaria de poder fazer passagem de parametros\
+             (temperatura) para o vertex shader, com o objetivo de aumentar a\
+             flexibilidade do meu codigo.";
+             
+            "Eu, enquanto usuario, gostaria de executar o codigo GLSL sobre um\
+             modelo carregado com o objetivo de gerar um resultado.";
+            
+            "Eu, enquanto usuario, gostaria de visualizar o resultado de um codigo\
+             atuando sobre um modelo com o objetivo de conferir o resultado.";
+            {
+                "Eu, enquanto usuario, gostaria de visualizar o resultado na\
+                 janela de visualizacao ativa no momento";
+                
+                "Eu, enquanto usuario, gostaria de visualizar o resultado em\
+                 uma nova janela de visualizacao";
+            }
+            
+            "Eu, enquanto usuario, gostaria de receber um feedback da compilacao\
+             do meu codigo com o objetivo de saber o que potencialmente pode\
+             estar errado no programa.";
+             
+        }
+
+         3.2;
+        "Eu, enquanto usuario, gostaria de testar codigo para Hull Shader\
+         assim como faco para o vertex shader.";
+        
+         3.2;
+        "Eu, enquanto usuario, gostaria de testar codigo para Domain Shader\
+         assim como faco para o vertex shader.";
+        
+         3.2;
+        "Eu, enquanto usuario, gostaria de testar codigo para Geometry Shader\
+         assim como faco para o vertex shader.";
+        
+         3.2;
+        "Eu, enquanto usuario, gostaria de testar codigo para Fragment Shader\
+         assim como faco para o vertex shader.";
+        
+         1;
+        "Eu, enquanto usuario, gostaria de escolher quais shaders abertos serao\
+         executados.";
+    }
+
+    9;
+    "Eu enquanto pesquisador, gostara de obter estati­sticas sobra a execucao do meu\
+     codigo GLSL com o objetivo de levantar dados para pesquisas cientificas.";
+
+    12;
+    "Eu, enquanto usuario, gostaria de dispor de um ambiente onde eu pudesse mexer\
+     nas configuracoes disponi­veis para GLSL e OpenGL com o objetivo de .......";
+
+    17;
+    "Eu, enquanto profissional, gostaria de dispor funcionalidades extras para GLSL\
+     com o objetivo de melhorar minha produtividade.";
+    {
+         7;
+        "Eu, enquanto profissional, gostaria de carregar plugins, como bjetivo de\
+         ter novas funcionalidades no programa.";
+
+         5;
+        "Eu, enquanto profissional, gostaria posicionar a iluminacao utilizando\
+         o mouse.";
+        {
+            "Eu, enquanto profissional, gostaria que o objeto iluminador fosse\
+             mostrado na visualizacao";
+
+            "Eu, enquanto profissional, gostaria selecionar e movimentar o objeto\
+             de iluminacao.";
+        }
+
+         4;
+        "Eu, enquanto profissional, gostaria de ter auto-completion no editor de\
+         codigo GLSL.";
+
+         1;
+        "Eu, enquanto profissional, gostaria de poder escolher entre varios\
+         modelos de manipulacao de camera.";
+    }
+
+    8;
+    "Eu, enquanto usuario, gostaria de carregar um modelo com objetivo de submeter\
+     o meu codigo a uma situacao especi­fica.";
+    {
+         3.5;
+        "Eu, enquanto usuario, gostaria de carregar um modelo dentre algumas opcoes\
+         padrao fornecidas pelo programa, com o objetivo de poder testar codigo\
+         sem dispor de um modelo proprio e rapidamente.";
+
+         4.5;
+        "Eu, enquanto usuario, gostaria de carregar um modelo proprio, com o\
+         objetivo de poder testar cadigo em situacoes especificas.";
+    }
+    
+    8;
+    "Eu, enquanto usuario, gostaria de utilizar texturas junto com meus programas,\
+     com o objetivo de ver como fica.";
+
+    8;
+    "Eu enquanto usuario, gostaria de poder reposicionar a camera da visualizacao\
+     dos resultados, com o objetivos de avalia-los por outros angulos.";
+
+    12;
+    "Eu enquanto pesquisador, gostaria de salvar o estado do programa com o\
+     objetivo de retomar minhas atividades posteriormente sem perder as\
+     configuracoes feitas.";
+
+    9;
+    "Eu, enquanto pesquisador, gostaria de gerar um executavel 'stand-alone' do meu\
+     codigo rodando sobre um modelo, como acontece na IDE, com o objetivo de\
+     leva-lo comigo e executa-lo em outros computadores mais facilmente.";
+}
+
+
+
+
+
+
+
+
+
+
+
+
