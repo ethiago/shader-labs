e3adf12a2fe6040800cc092dcc63b92da7b32342
e.thiago.06 <e.thiago.06@23c9b60f-9f39-85ba-2a90-a2d20fce122a>
2011-08-02 18:31:29 +0000

    Lista compilada para Plano e Esfera funcionnado no Windows e Linux
    Necessario testar em maquina de baixa performace de Placa de videos
    (LCI)

--- trunk/headers/Sphere.h
+++ trunk/headers/Sphere.h
@@ -8,21 +8,24 @@ class Sphere : public Object3D
     Q_OBJECT
 
     float m_radius;
+    int indexList;
 public:
     explicit Sphere(int stacks = 50, int slices = 50, float radius = 1.0,
                     const QVector3D& center = QVector3D(),
                     QObject *parent = 0);
     explicit Sphere(const Sphere& sph);
+    ~Sphere();
 
-    void setRadius(float radius);
     float radius(void)const;
 
 private:
     virtual void drawGeometry(void) const;
 
-signals:
+    void storeList();
 
-public slots:
+ protected:
+    void setRadius(float radius);
+
 
 };
 
