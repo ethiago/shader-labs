1b9bdd29fab5f074ec7f5dc6c1686674b45ad46a
e.thiago.06 <e.thiago.06@gmail.com>
2012-03-25 02:43:41 +0000

    Cubo flat

--- src/Cube.cpp
+++ src/Cube.cpp
@@ -35,6 +35,7 @@ void Cube::drawGeometry(void)const
 void Cube::storeList()const
 {
 
+    ////////////////////////////////////////////////////////////
     float v[24] = {   -1.0, -1.0,  1.0,
                        1.0, -1.0,  1.0,
                       -1.0,  1.0,  1.0,
@@ -44,78 +45,123 @@ void Cube::storeList()const
                       -1.0, -1.0, -1.0,
                        1.0, -1.0, -1.0};
 
-       float n[24] = {-0.57735, -0.57735,  0.57735,
-                       0.57735, -0.57735,  0.57735,
-                      -0.57735,  0.57735,  0.57735,
-                       0.57735,  0.57735,  0.57735,
-                      -0.57735,  0.57735, -0.57735,
-                       0.57735,  0.57735, -0.57735,
-                      -0.57735, -0.57735, -0.57735,
-                       0.57735, -0.57735, -0.57735};
+    float t[20] = { 0.0,  0.66,
+                    0.0,  0.33,
+                    0.25, 0.66,
+                    0.25, 0.33,
+                    0.5 , 0.66,
+                    0.5 , 0.33,
+                    0.75, 0.66,
+                    0.75, 0.33,
+                    1.0 , 0.66,
+                    1.0 , 0.33
+                   };
+    float t2[16]= { 0.25, 1.0,
+                    0.25, 0.0,
+                    0.25, 0.66,
+                    0.25, 0.33,
+                    0.5 , 0.66,
+                    0.5 , 0.33,
+                    0.5 , 1.0,
+                    0.5 , 0.0
+                 };
 
 
-       glNewList(indexList, GL_COMPILE);
-       {
-           glBegin(GL_QUAD_STRIP);
-           {
-               glNormal3fv(&n[3*0]);
-               glVertex3fv(&v[3*0]);
+  glColor4f( 0.35, 0.35, 0.35, 1.0);
 
-               glNormal3fv(&n[3*1]);
-               glVertex3fv(&v[3*1]);
+  glNewList(indexList, GL_COMPILE);
+  {
+      glBegin(GL_QUADS);
+      {
+          //F1
+          glNormal3f(0.0,0.0,1.0);
+          glTexCoord2fv(&t[2*0]);
+          glVertex3fv(&v[3*0]);
 
-               glNormal3fv(&n[3*2]);
-               glVertex3fv(&v[3*2]);
+          glTexCoord2fv(&t[2*1]);
+          glVertex3fv(&v[3*1]);
 
-               glNormal3fv(&n[3*3]);
-               glVertex3fv(&v[3*3]);
+          glTexCoord2fv(&t[2*3]);
+          glVertex3fv(&v[3*3]);
 
-               glNormal3fv(&n[3*4]);
-               glVertex3fv(&v[3*4]);
+          glTexCoord2fv(&t[2*2]);
+          glVertex3fv(&v[3*2]);
 
-               glNormal3fv(&n[3*5]);
-               glVertex3fv(&v[3*5]);
 
-               glNormal3fv(&n[3*6]);
-               glVertex3fv(&v[3*6]);
+          //F4
+          glNormal3f(0.0,1.0,0.0);
+          glTexCoord2fv(&t[2*2]);
+          glVertex3fv(&v[3*2]);
 
-               glNormal3fv(&n[3*7]);
-               glVertex3fv(&v[3*7]);
+          glTexCoord2fv(&t[2*3]);
+          glVertex3fv(&v[3*3]);
 
-               glNormal3fv(&n[3*0]);
-               glVertex3fv(&v[3*0]);
+          glTexCoord2fv(&t[2*5]);
+          glVertex3fv(&v[3*5]);
 
-               glNormal3fv(&n[3*1]);
-               glVertex3fv(&v[3*1]);
-           }glEnd();
+          glTexCoord2fv(&t[2*4]);
+          glVertex3fv(&v[3*4]);
 
-           glBegin(GL_QUADS);
-           {
-               glNormal3fv(&n[3*7]);
-               glVertex3fv(&v[3*7]);
 
-               glNormal3fv(&n[3*5]);
-               glVertex3fv(&v[3*5]);
+          //F0
+          glNormal3f(0.0,0.0,-1.0);
+          glTexCoord2fv(&t[2*4]);
+          glVertex3fv(&v[3*4]);
 
-               glNormal3fv(&n[3*3]);
-               glVertex3fv(&v[3*3]);
+          glTexCoord2fv(&t[2*5]);
+          glVertex3fv(&v[3*5]);
 
-               glNormal3fv(&n[3*1]);
-               glVertex3fv(&v[3*1]);
+          glTexCoord2fv(&t[2*7]);
+          glVertex3fv(&v[3*7]);
 
-               glNormal3fv(&n[3*0]);
-               glVertex3fv(&v[3*0]);
+          glTexCoord2fv(&t[2*6]);
+          glVertex3fv(&v[3*6]);
 
-               glNormal3fv(&n[3*2]);
-               glVertex3fv(&v[3*2]);
 
-               glNormal3fv(&n[3*4]);
-               glVertex3fv(&v[3*4]);
+          //F5
+          glNormal3f(0.0,-1.0,0.0);
+          glTexCoord2fv(&t[2*6]);
+          glVertex3fv(&v[3*6]);
 
-               glNormal3fv(&n[3*6]);
-               glVertex3fv(&v[3*6]);
+          glTexCoord2fv(&t[2*7]);
+          glVertex3fv(&v[3*7]);
 
-           }glEnd();
+          glTexCoord2fv(&t[2*9]);
+          glVertex3fv(&v[3*1]);
 
-       }glEndList();
+          glTexCoord2fv(&t[2*8]);
+          glVertex3fv(&v[3*0]);
+
+
+          //F2
+          glNormal3f(1.0,0.0,0.0);
+          glTexCoord2fv(&t2[2*7]);
+          glVertex3fv(&v[3*7]);
+
+          glTexCoord2fv(&t2[2*5]);
+          glVertex3fv(&v[3*5]);
+
+          glTexCoord2fv(&t2[2*3]);
+          glVertex3fv(&v[3*3]);
+
+          glTexCoord2fv(&t2[2*1]);
+          glVertex3fv(&v[3*1]);
+
+          //F3
+          glNormal3f(-1.0,0.0,0.0);
+          glTexCoord2fv(&t2[2*0]);
+          glVertex3fv(&v[3*0]);
+
+          glTexCoord2fv(&t2[2*2]);
+          glVertex3fv(&v[3*2]);
+
+          glTexCoord2fv(&t2[2*4]);
+          glVertex3fv(&v[3*4]);
+
+          glTexCoord2fv(&t2[2*6]);
+          glVertex3fv(&v[3*6]);
+
+      }glEnd();
+  }glEndList();
+
 }
