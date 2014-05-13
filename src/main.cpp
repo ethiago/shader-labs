a26bf825e5f1083afdfb501edfb7b34e6642445c
stratoluis <stratoluis@gmail.com>
2014-05-13 02:13:09 +0000

    GPLv3 blurb

--- src/main.cpp
+++ src/main.cpp
@@ -11,6 +11,16 @@ int main(int argc, char *argv[])
 {
     if(argc > 1)
     {
+        if ((!strcmp(argv[1],"-h")||!strcmp(argv[1],"--help")))
+        {
+            std::cout<<"Usage: "<<argv[0]<<" [option|slp file]"<<
+                       "\nwhere option is one of\n"<<
+                       "-h, --help\tprint this message and exit\n"<<
+                       "-v, --version\tprint version information and exit\n"<<
+                       "-l, --license\tshow information about the license"<<
+                       std::endl;
+            exit(-1);
+        }
         if ((!strcmp(argv[1],"-v")||!strcmp(argv[1],"--version")))
         {
             std::cout<<"ShaderLabs "<<
@@ -19,13 +29,19 @@ int main(int argc, char *argv[])
                        SL_VERSION_REV<<std::endl;
             exit(-1);
         }
-        if ((!strcmp(argv[1],"-h")||!strcmp(argv[1],"--help")))
+        if ((!strcmp(argv[1],"-l")||!strcmp(argv[1],"--license")))
         {
-            std::cout<<"Usage: "<<argv[0]<<" [option|slp file]"<<
-                       "\nwhere option is one of\n"<<
-                       "-h, --help\tprint this message and exit\n"<<
-                       "-v, --version\tprint version information and exit"<<
-                       std::endl;
+            std::cout<<
+                        "ShaderLabs "<<
+                        SL_VERSION_MAJOR<<'.'<<
+                        SL_VERSION_MINOR<<'.'<<
+                        SL_VERSION_REV<<
+                        "\nCopyright (C) 2011 Thiago E. Gomes & F. V. Vianna, "<<
+                        "2012-2014 Thiago E. Gomes\n"<<
+                        "This is free software; see the source for copying "<<
+                        "conditions.  There is NO\nwarranty; not even for "<<
+                        "MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE."<<
+                        std::endl;
             exit(-1);
         }
     }
