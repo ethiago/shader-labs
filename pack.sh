554bcbebf2b7fba4ee03f97491f49898531baf7e
ethiago <e.thiago.06@gmail.com>
2018-03-23 20:22:41 -0300

    Corrige CD

--- pack.sh
+++ pack.sh
@@ -2,9 +2,24 @@
 
 APPPATH="build/app"
 DEBPATH="$APPPATH/DEBIAN"
-BINNAME="shaderlabs"
-VERSION=$1
 BINPATH=usr/shaderlabs/shaderlabs-$VERSION/
+
+mkdir $DEBPATH
+
+cat > "$DEBPATH/control" << EOM
+Source: ShaderLabs
+Package: ShaderLabs
+Version: $VER
+Maintainer: Thiago Gomes <thiagoegomes@gmail.com>
+Section: graphics
+Priority: optional
+Architecture: all
+Depends: libqt4-core, libqt4-gui, libqt4-xml, libqt4-opengl, libglu1-mesa
+Description: ShaderLabs is a IDE for GLSL experimentation
+ ShaderLabs provides a abstraction over a OpenGL aplication so you can 
+ focus on learn or propotype your OpenGL vertex, fragment, geometry 
+ or tessellation shaders.
+EOM
 
 cat > "$DEBPATH/postinst" << EOM
 ln -sf /$BINPATH/$BINNAME /usr/$BINNAME
@@ -20,6 +35,6 @@ mv $APPPATH/lib $APPPATH/$BINPATH/
 dpkg-deb --build $APPPATH
 EXC=$?
 
-mv app.deb $BINNAME\_$VERSION.deb
+mv app.deb $BINNAME\_$VER.deb
 
 exit $EXC
