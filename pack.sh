ea1a198bbd9054c0b1c769bf34adf8edfa88e63a
ethiago <e.thiago.06@gmail.com>
2018-03-28 13:48:09 -0300

    Fix travis post compile routines

--- pack.sh
+++ pack.sh
@@ -14,7 +14,7 @@ Maintainer: Thiago Gomes <thiagoegomes@gmail.com>
 Section: graphics
 Priority: optional
 Architecture: all
-Depends: libqt4-core, libqt4-gui, libqt4-xml, libqt4-opengl, libglu1-mesa
+Depends: libglu1-mesa
 Description: ShaderLabs is a IDE for GLSL experimentation
  ShaderLabs provides a abstraction over a OpenGL aplication so you can 
  focus on learn or propotype your OpenGL vertex, fragment, geometry 
